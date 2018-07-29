599602a942dde838b203cdefefc5f3c039e03112
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-29 16:50:02 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,34 @@
 class Transfer
-  def initialize(transfer)
-    @transfer = transfer
+attr_accessor :sender, :receiver, :amount, :status
+def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = "pending"
+end 
+def valid?
+  @sender.valid? && receiver.valid?
+end 
+def execute_transaction
+  if valid? && @sender.balance > amount && status == "pending"
+    @sender.balance -= amount
+    @receiver.balance += amount
+    @status = "complete"
+  else 
+    reject_transfer
   end 
-end
+end 
+def reject_transfer
+  @status = "rejected"
+  "Transaction rejected. Please check your account balance."
+end 
+def reverse_transfer 
+  if valid? && @receiver.balance > amount && status == "complete"
+    @receiver.balance -= amount
+    @sender.balance += amount
+    @status = "reversed"
+  else 
+    reject_transfer
+  end 
+end 
+end 
